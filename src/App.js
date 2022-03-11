532e57e6dee633412e2296c97f2b4fbde92dae72
Ramires60 <ramires60@gmail.com>
2022-03-11 23:47:13 +0200

    new13

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Content from './Content/content';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className='header'> <Header /> </header>
+      <nav className='nav'> <Sidebar /> </nav>
+      <div className='content'><Content /></div>
+      <div className='footer'><Footer /></div>
+   
     </div>
   );
 }
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Sidebar = () => {
+    return (
+        <div>
+        <div>Nav1</div>
+        <div>Nav2</div>
+        <div>Nav3</div>
+        </div>
+    )
+}
+
+export default Sidebar
